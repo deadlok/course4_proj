de9d302bda61411ec83e6861cbaee733821a6216
man lok lee <lok@pg95.com>
2023-06-01 09:04:02 +0000

    Finish celery introduction

--- course4_proj/django_client.py
+++ course4_proj/django_client.py
@@ -5,3 +5,4 @@ from omdb.client import OmdbClient
 def get_client_from_settings():
     """Create an instance of an OmdbClient using the OMDB_KEY from the Django settings."""
     return OmdbClient(settings.OMDB_KEY)
+
--- course4_proj/settings.py
+++ course4_proj/settings.py
@@ -51,6 +51,7 @@ class Dev(Configuration):
         'movies',
         'omdb',
         'gh',
+        'django_celery_results',
     ]
 
     MIDDLEWARE = [
@@ -161,3 +162,6 @@ class Dev(Configuration):
     }        
 
     OMDB_KEY = "152bae90"
+
+    CELERY_RESULT_BACKEND = "django-db"
+    CELERY_BROKER_URL = "redis://localhost:6379/0"
